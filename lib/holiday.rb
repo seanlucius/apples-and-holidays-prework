79b34a51720b84b11bd2e038fb77b55233fcdb3c
Sean Lucius <seanlucius22@icloud.com>
2019-01-24 23:21:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,11 +49,15 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq_holidays = []
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, list|
-      if list
+      if list.include?("BBQ")
+        bbq_holidays.push(holiday)
+      end 
     end 
   end 
+  bbq_holidays
 end
 
 
