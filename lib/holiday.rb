4401b999d4bf6e42ca6123325694234065a76a99
Sean Lucius <seanlucius22@icloud.com>
2019-01-24 22:20:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,7 +34,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{(season.to_s).capitalize}:"
     holiday.each do |holiday, list|
-      current_supplies = holiday.values
+  
       puts "  #{(holiday.to_s).capitalize}: #{current_supplies}"
     end 
   end 
